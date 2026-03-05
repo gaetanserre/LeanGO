da2acfc2dc613ab1a7a2e3bd4c49af136a9b3f48
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-03-05 18:19:59 +0100

    Authoring

--- LeanGO.lean
+++ LeanGO.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 Gaëtan Serré. All rights reserved.
+Released under GNU GPL 3.0 license as described in the file LICENSE.
+Authors: Gaëtan Serré
+-/
+
 import LeanGO.Algorithm
 import LeanGO.Utils.Fintype
 import LeanGO.Utils.Iter
