7f9ea954822a49240d41c00654eaf49238e2c1fc
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-05-29 18:17:47 +0200

    typo

--- LeanGO/verso/LeanGOManual/Examples/Decision.lean
+++ LeanGO/verso/LeanGOManual/Examples/Decision.lean
@@ -26,6 +26,6 @@ htmlSplit := .never
 
 We provide an interface for _decision-based optimization algorithms_. Such algorithm are described by a sequence of decision rules to decide wether to accept or reject a candidate solution at each iteration, based on the observed data. Decision rules are functions with the signature $`D_n : \Omega^{n + 1} \times \beta^{n + 1} \to [0, 1]`, where $`\Omega` is the search space and $`\beta` is the space of function values (most likely $`\mathbb{R}`). More formally, given a function to optimize $`V : \Omega \to \beta` at each iteration $`n > 0`, the algorithm samples a candidate $`x` from a distribution $`\mu` on $`\Omega` and accepts it with probability $`D_n(C_n, V(C_n), x)`.
 
-We define the {name Decision}`Decision` structure as a special case of the `Algorithm` structure, for which the Markov kernel at iteration $`n` is defined through the decision rules $`(D_n)_{n \in \mathbb{N}}`. Measurability assumptions are required on the decision rules for the kernels to be well-defined and we only support $`\{0, 1\}`-valued decision rules at the moment.
+We define {name Decision}`Decision` as a special case of the `Algorithm` structure, for which the Markov kernel at iteration $`n` is defined through the decision rules $`(D_n)_{n \in \mathbb{N}}`. Measurability assumptions are required on the decision rules for the kernels to be well-defined and we only support $`\{0, 1\}`-valued decision rules at the moment.
 
 {docstring Decision}
